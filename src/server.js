eb730342d8b7caa4a7f6f919772caefd4fa34958
mariaati <mariaatia36@gmail.com>
2023-09-27 16:26:29 +0300

    added comment

--- src/server.js
+++ src/server.js
@@ -9,6 +9,9 @@ CORS (Cross-Origin Resource Sharing) is a browser security feature that restrict
 cross-origin HTTP requests with other servers and specifies which domains access your resources.
 We will use this Node.js package to allow cross-origin requests.
  */
+
+
+
 app.use(cors());
 app.use(express.json());
 // enables the server to serve the client app without running it
@@ -26,3 +29,5 @@ app.get('/*', (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+/*hiii/
